4f34296a68e07ca6fd8fc458da49aa35a4cd712f
Yiqi Zhang <>
2020-04-13 11:04:02 -0400

    setup

---
+++ src/App.styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const PageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+`;
