bc80dc2840b535aeb31fb4fb82ad86c67416a7a7
Yiqi Zhang <>
2020-04-14 12:54:36 -0400

    home updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,22 +4,7 @@ import KardSelectors from "./KardSelectors";
 const Home = () => {
   return (
     <>
-
-    <BrowserRouter>
-      <Link to="/book">Book</Link>
-
-      <Switch>
-        <Route path="/book">
-          <Book />
-        </Route>
-      </Switch>
-    </BrowserRouter>
-{/* 
-      <KardContainer>Album</KardContainer>
-      <KardContainer>Restaurant</KardContainer>
-      <KardContainer>City</KardContainer>
-      <KardContainer>Books</KardContainer> */}
-
+      <KardSelectors></KardSelectors>
     </>
   );
 };
