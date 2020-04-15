ecaf90c69a725766cd0b218f95f4b7b37a4aa26f
tom <lim1017@hotmail.com>
2020-04-15 00:11:59 -0400

    pictures

--- src/components/Movie/AllMovieResults.js
+++ src/components/Movie/AllMovieResults.js
@@ -12,9 +12,12 @@ const AllMovieResults = ({ movies }) => {
   return (
     <SearchDiv className="row">
       {movies.results.map(movie => (
-       
+       <>
        <h1>{movie.title}</h1>
-
+        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              height="200px"
+        />
+        </>
       ))}
     </SearchDiv>
   );
