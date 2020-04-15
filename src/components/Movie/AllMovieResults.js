835b7470ed797057959aa7d3a2752510d9adf5ee
tom <lim1017@hotmail.com>
2020-04-15 17:15:04 -0400

    1/2 movies

--- src/components/Movie/AllMovieResults.js
+++ src/components/Movie/AllMovieResults.js
@@ -1,5 +1,5 @@
 import React from "react";
-import SearchResult from "../SearchResult";
+import SearchResultMovie from "./SearchResultMovie";
 import PropTypes from "prop-types";
 
 import { SearchDiv } from "../../App.styles";
@@ -12,12 +12,9 @@ const AllMovieResults = ({ movies }) => {
   return (
     <SearchDiv className="row">
       {movies.results.map(movie => (
-       <>
-       <h1>{movie.title}</h1>
-        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-              height="200px"
-        />
-        </>
+       
+       <SearchResultMovie movieData={movie}/>
+
       ))}
     </SearchDiv>
   );
---
+++ src/components/Movie/SearchResultMovie.js
@@ -0,0 +1,92 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const SearchResultMovie = (props) => {
+  /**
+   * truncate book title to first 4 words and append it with '...'
+   * indicating it is truncated.
+   * Full title will be shown in a tooltip
+   */
+
+  const { movieData } = props
+
+   console.log(movieData)
+
+  const movieTitle = movieData.original_title;
+  let displayTitle = movieTitle
+    .split(" ")
+    .slice(0, 6)
+    .join(" ");
+  if (movieTitle.length > displayTitle.length) {
+    displayTitle += "...";
+  }
+
+  return (
+    <div 
+   
+    className="col-lg-2 col-sm-4 col-md-3">
+      <div className="card">
+        <img
+          style={{ display:'flex ',
+            margin:'auto'
+          }} 
+          className="card-img-top pl-2 pr-2 pt-2"
+          src={`https://image.tmdb.org/t/p/w500${movieData.poster_path}`}
+          alt="Book cover"
+          height="200px"
+        />
+        <div className="card-body"
+             style={{ display:'flex ',
+             flexDirection:'column',
+             margin:'auto'
+             }}
+        >
+          <p
+            className="text-sm-left card-title font-weight-bold"
+            data-toggle="tooltip"
+            data-placement="bottom"
+            title={displayTitle.includes("...") ? movieTitle : ""}
+            style={{
+              display:"flex",
+              justifyContent:"center",
+              marginTop:"8px",
+              marginBottom: '8px'
+            }}
+          >
+            {displayTitle}
+          </p>
+        
+
+          <div 
+              style={{
+              display:"flex",
+              justifyContent:"center",
+              marginBottom:"1em"
+              }}>
+
+            <button
+              className="btn btn-primary"
+              onClick={() => console.log('select')}
+            >
+              Select
+            </button>
+
+            <button
+              className="btn btn-primary"
+              onClick={() => console.log('mor einfo')}
+            >
+              More Info
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+SearchResultMovie.propTypes = {
+  MovieData: PropTypes.object,
+  expandBook: PropTypes.func
+};
+
+export default SearchResultMovie;
