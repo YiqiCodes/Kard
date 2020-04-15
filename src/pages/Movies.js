eed2bb0df77841fce39a4d079a8720eec359f16f
tom <lim1017@hotmail.com>
2020-04-14 23:19:20 -0400

    movie api not working

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React from 'react';
+import SearchMovie from "../components/SearchMovie";
+import { BookDiv } from "../App.styles";
 
-const Movies = () => {
-  return (
-    <>
-      <div>Heat</div>
-    </>
-  );
-};
 
+
+class Movies extends React.Component {
+
+ render() {
+   return (
+    <BookDiv style={{height:'100%'}} className="container">
+    <div className="header clearfix mt-5">
+      <h3 style={{display:'flex', justifyContent:'center'}} className="text-muted">Movie Search</h3>
+    </div>
+    <div className="jumbotron">
+      <SearchMovie />
+    </div>
+  </BookDiv>
+   );
+ }
+}
 export default Movies;
