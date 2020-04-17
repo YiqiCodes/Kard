e40fbe25f29d0458540baf48edea15b577350fb5
JehyunRa <jra4862@gmail.com>
2020-04-17 17:16:41 -0400

    initial

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,15 +3,41 @@
 import React from "react";
 import { useAuth0 } from "../react-auth0-spa";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 
 const NavBar = () => {
   const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
+  const saveData = function(data) {
+    console.log(data);
+    // axios
+    // .put(`http://localhost:8001/api/budget/${user}`, newBud)
+    // .then(res1 => {
+    //   axios.get(`http://localhost:8001/api/budget/${user}`).then(res2 => {
+    //     dispatch({
+    //       ...state,
+    //       type: "SET_DATA",
+    //       budget: res2.data
+    //     });
+
+    //     if (state.budget.length === 0) {
+    //       updateLit();
+    //     }
+    //   });
+    // })
+    // .catch(error => {
+    //   console.log(error);
+    // });
+  }
+
   return (
     <div>
       {!isAuthenticated && (
+        <>
         <button onClick={() => loginWithRedirect({})}>Log in</button>
+        <button onClick={() => SaveData(props.faveAlbum)}>Save</button>
+        </>
       )}
 
       {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
