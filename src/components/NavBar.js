fddc1589bf3a8235413817e2f1c7d210e64c6199
JehyunRa <jra4862@gmail.com>
2020-04-17 18:05:55 -0400

    save

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,38 +6,30 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 
-const NavBar = () => {
-  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+const NavBar = (props) => {
+  const { isAuthenticated, loginWithRedirect, logout, user } = useAuth0();
 
-  const saveData = function(data) {
-    console.log(data);
-    // axios
-    // .put(`http://localhost:8001/api/budget/${user}`, newBud)
-    // .then(res1 => {
-    //   axios.get(`http://localhost:8001/api/budget/${user}`).then(res2 => {
-    //     dispatch({
-    //       ...state,
-    //       type: "SET_DATA",
-    //       budget: res2.data
-    //     });
+  const saveData = function() {
+    const dataForm = {
+      categories: { "album": props.faveAlbum, "book": props.faveBook, "movie": props.faveMovie, "resto": props.faveResto },
+      favorites: { "fav":"album book movie resto" },
+      name: user.email
+    }
 
-    //     if (state.budget.length === 0) {
-    //       updateLit();
-    //     }
-    //   });
-    // })
-    // .catch(error => {
-    //   console.log(error);
-    // });
+    axios
+    .put(`http://localhost:8001/api/users`, dataForm)
+    .then(res1 => {
+      console.log(res1.data)
+    })
+    .catch(error => {
+      console.log(error);
+    });
   }
 
   return (
     <div>
       {!isAuthenticated && (
-        <>
         <button onClick={() => loginWithRedirect({})}>Log in</button>
-        <button onClick={() => SaveData(props.faveAlbum)}>Save</button>
-        </>
       )}
 
       {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
@@ -46,6 +38,7 @@ const NavBar = () => {
       <span>
         <Link to="/">Home</Link>&nbsp;
         <Link to="/profile">Profile</Link>
+        <button onClick={() => saveData()}>Save</button>
       </span>
     )}
     
