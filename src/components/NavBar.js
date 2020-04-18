a262ef18f5e6cc1f960683050294b8bbd0381c60
Yiqi Zhang <>
2020-04-18 16:49:07 -0400

    updated navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,43 +5,115 @@ import { useAuth0 } from "../react-auth0-spa";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
+import "./NavBar.css";
 
 const NavBar = (props) => {
   const { isAuthenticated, loginWithRedirect, logout, user } = useAuth0();
 
-  const saveData = function() {
+  const saveData = function () {
     const dataForm = {
-      categories: { "album": props.faveAlbum, "book": props.faveBook, "movie": props.faveMovie, "resto": props.faveResto },
-      favorites: { "fav":"album book movie resto" },
-      name: user.email
-    }
+      categories: {
+        album: props.faveAlbum,
+        book: props.faveBook,
+        movie: props.faveMovie,
+        resto: props.faveResto,
+      },
+      favorites: { fav: "album book movie resto" },
+      name: user.email,
+    };
 
     axios
-    .put(`http://localhost:8001/api/users`, dataForm)
-    .then(res1 => {
-      console.log(res1.data)
-    })
-    .catch(error => {
-      console.log(error);
-    });
-  }
+      .put(`http://localhost:8001/api/users`, dataForm)
+      .then((res1) => {
+        console.log(res1.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        border: "1px solid black",
+        background: "black",
+        padding: "5px",
+      }}
+    >
+      {isAuthenticated && (
+        <span>
+          <Link
+            className="NavBar-Button"
+            style={{
+              color: "white",
+              font: "400 15px Arial",
+              textDecoration: "none",
+            }}
+            to="/"
+          >
+            Home
+          </Link>
+
+          <Link
+            className="NavBar-Button"
+            style={{
+              marginLeft: "1rem",
+              color: "white",
+              font: "400 15px Arial",
+              textDecoration: "none",
+            }}
+            to="/profile"
+          >
+            Profile
+          </Link>
+          <button
+            className="NavBar-Button"
+            style={{
+              marginLeft: "1rem",
+              color: "white",
+              font: "400 15px Arial",
+              textDecoration: "none",
+            }}
+            onClick={() => saveData()}
+          >
+            Save Kard
+          </button>
+
+          <Link
+            className="NavBar-Button"
+            style={{
+              marginLeft: "1rem",
+              color: "white",
+              font: "400 15px Arial",
+              textDecoration: "none",
+            }}
+            to="/about"
+          >
+            About
+          </Link>
+        </span>
+      )}
       {!isAuthenticated && (
-        <button onClick={() => loginWithRedirect({})}>Log in</button>
+        <button
+          className="NavBar-Button"
+          style={{ color: "white" }}
+          onClick={() => loginWithRedirect({})}
+        >
+          Log In
+        </button>
       )}
 
-      {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
-
       {isAuthenticated && (
-      <span>
-        <Link to="/">Home</Link>&nbsp;
-        <Link to="/profile">Profile</Link>
-        <button onClick={() => saveData()}>Save</button>
-      </span>
-    )}
-    
+        <button
+          className="NavBar-Button"
+          style={{ color: "white" }}
+          onClick={() => logout()}
+        >
+          Log Out
+        </button>
+      )}
     </div>
   );
 };
